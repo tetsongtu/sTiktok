37c2411b38887af73f55c1801789219db3195f96
tetsongtu <2505letdo@gmail.com>
2026-01-04 22:52:59 +0700

    .

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	let { children, className = '', rounded = false, primary = false, text = false } = $props();
-
-	let hoverText = $derived(text ? 'hover:text-gray-600' : ' hover:bg-gray-100 py-1.5');
-	let shape = $derived(rounded ? 'rounded-full' : 'rounded-lg');
-	let color = $derived(primary ? 'bg-rose-500 hover:bg-rose-600 text-white justify-center' : '');
-	let classes = $derived(
-		`flex items-center ${className} ${hoverText} ${shape} ${color} cursor-pointer`
-	);
 </script>
 
-<button class={classes}>
+<button
+	class={`${className} flex items-center cursor-pointer 
+	${text ? 'hover:text-gray-600' : 'hover:bg-gray-100 py-1.5'} 
+	${primary && 'bg-rose-500 hover:bg-rose-600 text-white justify-center'}
+	${rounded ? 'rounded-full' : 'rounded-lg'}`}
+>
 	{@render children()}
 </button>
