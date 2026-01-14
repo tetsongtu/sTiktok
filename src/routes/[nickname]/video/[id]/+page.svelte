3d148cc189057738b214868478d4f47b41f64d66
tetsongtu <2505letdo@gmail.com>
2026-01-15 00:48:05 +0700

    The video ID is being read from query parameters instead of the URL path.

--- src/routes/[nickname]/video/[id]/+page.svelte
+++ src/routes/[nickname]/video/[id]/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import HomePage from '../../../+page.svelte';
+	import HomePage from '$src/routes/+page.svelte';
 	import { comment, activeVideo } from '$lib/stores/index.svelte';
 	import { onMount } from 'svelte';
 	import { page } from '$app/state';
 
 	onMount(() => {
 		comment.open = true;
-		activeVideo.id = Number(page.url.searchParams.get('video'));
+		activeVideo.id = Number(page.params.id);
 	});
 </script>
 
